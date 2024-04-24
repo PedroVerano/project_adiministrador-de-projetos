ae82a58b3733c3c6289c72c761a0863c08860b2d
Pedro Verano <83721678+PedroVerano@users.noreply.github.com>
2024-04-24 09:06:00 -0300

    Adiciona o componente de Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,27 @@
+import {FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+import styles from './Footer.module.css';
+
 function Footer() {
     return(
-        <p>Footer</p>
+        <footer>
+            <ul>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+                <li>
+                    <FaGithub />
+                </li>
+            </ul>
+            <p>
+                <span>Costs</span> &copy; 2021
+            </p>
+        </footer>
     )
 }
 
