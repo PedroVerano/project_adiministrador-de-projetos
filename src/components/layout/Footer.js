1a37c97c8826fcff3cb29cd4129e84df60e234db
Pedro Verano <83721678+PedroVerano@users.noreply.github.com>
2024-04-24 09:21:59 -0300

    Estilizando o Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,24 +1,25 @@
 import {FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 import styles from './Footer.module.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return(
-        <footer>
-            <ul>
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
                 <li>
-                    <FaFacebook />
+                    <Link to='https://www.facebook.com/pedro.verano.73'  target="_blank"> <FaFacebook /> </Link>
                 </li>
                 <li>
-                    <FaInstagram />
+                    <Link to='https://www.instagram.com/peeverano/'  target="_blank"> <FaInstagram /> </Link>   
                 </li>
                 <li>
-                    <FaLinkedin />
+                    <Link to=''  target="_blank"> <FaLinkedin /> </Link> 
                 </li>
                 <li>
-                    <FaGithub />
+                    <Link to='https://github.com/PedroVerano'  target="_blank"> <FaGithub /> </Link> 
                 </li>
             </ul>
-            <p>
+            <p className={styles.copy}>
                 <span>Costs</span> &copy; 2021
             </p>
         </footer>
