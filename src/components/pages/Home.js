1156c0501ad279852db36ae647957837ccd7d243
Pedro Verano <83721678+PedroVerano@users.noreply.github.com>
2024-04-25 07:21:22 -0300

    Estilizando a Home e criando componente de LinkButton

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,14 @@
 import styles from './Home.module.css';
 import LogoPorco from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton';
 function Home () {
     return(
-        <section>
+        <section className={styles.home_container}>
             <h1>
                 Bem vindo ao <span>Coats</span>
             </h1>
             <p>Comece a gerenciar seus projetos agora mesmo!</p>
-            <a href="/">Criar Projeto</a>
+            <LinkButton to='/novoprojeto' text='Criar Projeto' />
             <img src={LogoPorco} alt="Cofrinho de porco com um homem cavalgando. Coats"></img>
         </section>
     )
