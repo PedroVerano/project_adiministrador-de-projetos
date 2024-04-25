eeafdea1c3e9e243a2c1b5a8590778d6e0136e76
Pedro Verano <83721678+PedroVerano@users.noreply.github.com>
2024-04-25 07:06:57 -0300

    Componente Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from './Home.module.css';
+import LogoPorco from '../../img/savings.svg'
 function Home () {
     return(
-        <h1>Home</h1>
+        <section>
+            <h1>
+                Bem vindo ao <span>Coats</span>
+            </h1>
+            <p>Comece a gerenciar seus projetos agora mesmo!</p>
+            <a href="/">Criar Projeto</a>
+            <img src={LogoPorco} alt="Cofrinho de porco com um homem cavalgando. Coats"></img>
+        </section>
     )
 }
 export default Home;
