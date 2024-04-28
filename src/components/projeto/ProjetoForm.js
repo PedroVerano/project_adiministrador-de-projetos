28ba5dbbeb1de39078ee4ec877c36f5cc7bc7122
Pedro Verano <83721678+PedroVerano@users.noreply.github.com>
2024-04-28 10:39:48 -0300

    Adicionando componente de Input no forms

--- src/components/projeto/ProjetoForm.js
+++ src/components/projeto/ProjetoForm.js
@@ -1,7 +1,19 @@
+import Input from '../form/Input';
+import styles from './ProjetoForm.module.css';
+
 function ProjetoForm() {
     return(
-        <div>
-
+        <div className={styles.form}>
+            <Input 
+                type='text' 
+                name='name' 
+                text='Nome do projeto'
+                placeholder='Insira o nome do projeto' />
+            <Input 
+                type='number' 
+                name='budget' 
+                text='Orçamento do projeto'
+                placeholder='Insira o orçamento do projeto' />
         </div>
     )
 }
