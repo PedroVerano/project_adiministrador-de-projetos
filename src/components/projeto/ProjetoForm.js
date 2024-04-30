9dc50d0db26d23039dea51126ba3c1aa17ffb45c
Pedro Verano <83721678+PedroVerano@users.noreply.github.com>
2024-04-30 08:39:38 -0300

    Adicionando "Banco de dados"
    Adiciona um db fake para o projeto, armazenando categorias do novo projeto e futuramente os projetos adicionados

--- src/components/projeto/ProjetoForm.js
+++ src/components/projeto/ProjetoForm.js
@@ -1,9 +1,19 @@
+import {useState, useEffect} from 'react'
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjetoForm.module.css';
 
 function ProjetoForm({btnText}) {
+
+    const [categorias, setCategorias] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/categorias').then((response) => response.json())
+    .then((data) => {
+        setCategorias(data);
+    })
+    .catch((err) => console.log(err));
+    }, []);
     return(
         <div className={styles.form}>
             <Input 
@@ -17,7 +27,7 @@ function ProjetoForm({btnText}) {
                 text='Orçamento do projeto'
                 placeholder='Insira o orçamento do projeto' />
             
-            <Select name='categotia_id' text='Selecione a categoria' />
+            <Select name='categotia_id' text='Selecione a categoria' options={categorias}/>
             <SubmitButton text={btnText}/>
         </div>
     )
